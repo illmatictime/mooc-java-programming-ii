dcc706376422b80440e9c5230b7ec6e53ba96775
illmatictime <jerrylanderos@gmail.com>
2024-01-02 13:19:17 -0800

    Finished 8.04

--- part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -4,18 +4,48 @@ import java.util.Scanner;
 public class LiquidContainers2 {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)) {
+            Container container0 = new Container();
+            Container container1 = new Container();
 
+            while (true) {
+                System.out.println("> ");
+                System.out.println("First: " + container0);
+                System.out.println("Second: " + container1);
 
-        while (true) {
-            System.out.print("> ");
+                String input = scan.nextLine();
+                if (input.equals("quit")) {
+                    break;
+                }
 
-            String input = scan.nextLine();
-            if (input.equals("quit")) {
-                break;
+                String[] parts = input.split(" ");
+                String command = parts[0];
+                int amount = Integer.valueOf(parts[1]);
+
+                if(command.equals("add") && amount>0){
+                    container0.add(amount);
+                }else if(command.equals("move") && amount>0){
+                    if(container0.contains()>=amount){
+                        container1.add(amount);
+                        container0.remove(amount);;
+                    }else if(amount>container0.contains()){
+                        container1.add(container0.contains());;
+                        container0.remove(amount);;    
+                    }
+                    if(container1.contains() >= 100){
+                        container1.add(amount);;
+                    }
+
+                }else if(command.equals("remove") && amount>0){
+                    container1.remove(amount);
+                }
+
             }
-
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+
     }
 
 }
