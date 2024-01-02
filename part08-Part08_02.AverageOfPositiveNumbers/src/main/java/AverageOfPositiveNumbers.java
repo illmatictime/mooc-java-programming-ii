6c4f7e31b3d7bb4f1de01d6f7e22b55f616346c8
illmatictime <jerrylanderos@gmail.com>
2024-01-01 22:06:58 -0800

    Finished 8.02

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -4,7 +4,31 @@ import java.util.Scanner;
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            int count = 0;
+            int sum = 0;
+
+            while (true) {
+                int input = Integer.valueOf(scanner.nextLine());
+
+                if (input == 0) {
+                    break;
+                } else if (input > 0) {
+                    sum += input;
+                    count++;
+                }
+
+            }
+
+            if (count < 1) {
+                System.out.println("Cannot calculate the average");
+            } else {
+                System.out.println(1.0 * sum / count);
+            }
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
 }
