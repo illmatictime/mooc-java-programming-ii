96ab60ed13fa5b8abb0711a57f504e51591062ca
illmatictime <jerrylanderos@gmail.com>
2024-01-03 11:15:35 -0800

    Finished 8.09

--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -1,10 +1,34 @@
 
 import java.util.HashMap;
 
+
 public class Program {
 
     public static void main(String[] args) {
         // Test your program here!
+        HashMap<String, Book> hashmap = new HashMap<>();
+        hashmap.put("sense", new Book("Sense and Sensibility", 1811, "..."));
+        hashmap.put("prejudice", new Book("Pride and prejudice", 1813, "...."));
+        
+        printValues(hashmap);
+        System.out.println("---");
+        printValueIfNameContains(hashmap, "prejud");
     }
+
+    public static void printValueIfNameContains(HashMap<String, Book> hashmap, String string) {
+        for (Book name : hashmap.values()) {
+            if(name.getName().contains(string)){
+                System.out.println(name);
+            }
+        }
+    }
+
+    public static void printValues(HashMap<String,Book> hashmap){
+        for (Book valueBook : hashmap.values()) {
+            System.out.println(valueBook);
+        }
+    }
+
+    
 
 }
