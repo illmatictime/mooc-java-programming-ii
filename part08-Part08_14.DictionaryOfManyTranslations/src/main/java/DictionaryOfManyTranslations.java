b8588172be50934ce5050cc876e6e2d410489505
illmatictime <jerrylanderos@gmail.com>
2024-01-07 02:50:39 -0800

    Update 8.14

--- part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
+++ part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -3,14 +3,18 @@ import java.util.HashMap;
 
 public class DictionaryOfManyTranslations {
 
-    private HashMap<String, ArrayList<String>> translations;
+    private HashMap<String, ArrayList<String>> dictionary;
 
-    public DictionaryOfManyTranslation() {
-        this.translations = new HashMap<>();
+    public DictionaryOfManyTranslations() {
+        this.dictionary = new HashMap<>();
     }
 
     public void add(String word, String translation) {
+        this.dictionary.putIfAbsent(word, new ArrayList<>());
 
+        ArrayList<String> translations = this.dictionary.get(word);
+
+        translations.add(translation);
     }
 
     public ArrayList<String> translate(String word) {
