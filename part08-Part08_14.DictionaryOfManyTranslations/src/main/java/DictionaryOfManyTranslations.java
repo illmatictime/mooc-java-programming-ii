881166f200482009482f9626f21b59725a2f2c4d
illmatictime <jerrylanderos@gmail.com>
2024-01-06 19:47:04 -0800

    Update to 8.14

---
+++ part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DictionaryOfManyTranslations {
+
+    private HashMap<String, ArrayList<String>> translations;
+
+    public DictionaryOfManyTranslation() {
+        this.translations = new HashMap<>();
+    }
+
+    public void add(String word, String translation) {
+
+    }
+
+    public ArrayList<String> translate(String word) {
+        return null;
+
+    }
+
+    public void remove(String word) {
+
+    }
+}
