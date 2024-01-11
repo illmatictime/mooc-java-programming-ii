f34ad15c4d457f71e802213ddcc77114827b0e07
illmatictime <jerrylanderos@gmail.com>
2024-01-11 00:14:49 -0800

    Update 9.03

--- part09-Part09_03.Warehousing/src/main/java/ProductWarehouseWithHistory.java
+++ part09-Part09_03.Warehousing/src/main/java/ProductWarehouseWithHistory.java
@@ -1,3 +1,14 @@
-public class ProductWarehouseWithHistory {
+public class ProductWarehouseWithHistory extends ProductWarehouse {
 
+    private double initialBalance;
+
+    public ProductWarehouseWithHistory(String productName, double capacity, double initialBalance) {
+        super(productName, capacity);
+        this.initialBalance = super.getBalance();
+    }
+
+    public String history() {
+
+        return System.out.println(super.getBalance());
+    }
 }
