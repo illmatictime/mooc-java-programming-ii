f1b43021a00c26e0911e86e42963655b5e21046b
illmatictime <jerrylanderos@gmail.com>
2024-01-18 16:48:18 -0800

    Updated 9.10 7/9

---
+++ part09-Part09_10.OnlineShop/src/main/java/ShoppingCart.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingCart {
+    
+    private Map<String, Item> shoppingCart;
+
+    public ShoppingCart(){
+        this.shoppingCart = new HashMap<>();
+    }
+
+    public void add(String product, int price){
+        this.shoppingCart.put(product, new Item(product, 1, price));
+    }
+
+    public int price(){
+        int totalShoppingCartPrice = 0;
+        for (Item oneItemPrice : this.shoppingCart.values()) {
+            totalShoppingCartPrice += oneItemPrice.price();
+        }
+        return totalShoppingCartPrice;
+    }
+
+    public void print(){
+        for (Item oneItemPrice : this.shoppingCart.values()) {
+            System.out.println(oneItemPrice);
+        }
+    }
+}
