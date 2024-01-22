4d1e071135939979a073bdd9b209bc7ac17ee217
illmatictime <jerrylanderos@gmail.com>
2024-01-22 15:17:48 -0800

    Finished 10.02

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -5,8 +5,44 @@ import java.util.Scanner;
 public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        try (Scanner scanner = new Scanner(System.in)) {
+            ArrayList<String> input = new ArrayList<>();
+
+            System.out.println("Input numbers, type \"end\" to stop.");
+            while (true) {
+                String row = scanner.nextLine();
+                if(row.equals("end")){
+                    System.out.println("");
+                    break;
+                }
+
+                input.add(row);
+                //input.add(Integer.valueOf(read));
+            }
+            
+
+
+            double averageOfNegativeNumbers = input.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(number -> number < 0)
+                .average()
+                .getAsDouble();
+            
+            double averageOfPositiveNumbers = input.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(number -> number >= 0)
+                .average()
+                .getAsDouble();
+
+            System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+            String option = scanner.nextLine();
+
+            if(option.equals("n")){
+                System.out.println("Average of the negative numbers: " + averageOfNegativeNumbers);
+            }else if(option.equals("p")){
+                System.out.println("Average of the positive numbers: " + averageOfPositiveNumbers);
+            }
+        }
 
     }
 }
