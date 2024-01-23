837037ff97e3e9339f414509c6647af8dcfd75d4
illmatictime <jerrylanderos@gmail.com>
2024-01-22 16:45:29 -0800

    Finished 10.03

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -7,8 +7,14 @@ public class PositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test your method here
 
+        
+
+    }
+
+    public static List<Integer> positive(List<Integer> numbers){
+        return numbers.stream().filter(num -> num > 0)
+            .collect(Collectors.toList());
     }
 
 }
