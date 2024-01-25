6aef941d09edaebd956f53b94f1df44900599d3b
illmatictime <jerrylanderos@gmail.com>
2024-01-25 15:58:48 -0800

    Finished 10.06

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,19 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> input = new ArrayList<>();
+
+        while(true){
+            int row = Integer.valueOf(scanner.nextLine());
+            if(row < 0){
+                break;
+            }
+            input.add(row);
+        }
+
+        input.stream()
+            .filter(x -> x >= 1 & x <= 5)
+            .forEach(x -> System.out.println(x));
 
     }
 }
