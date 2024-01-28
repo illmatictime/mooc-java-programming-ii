a02f3bcf49d27719fd3aee46018f1b1b15d8f2aa
illmatictime <jerrylanderos@gmail.com>
2024-01-28 03:09:53 -0800

    Finished 8.14

--- part10-Part10_14.Literature/src/main/java/MainProgram.java
+++ part10-Part10_14.Literature/src/main/java/MainProgram.java
@@ -1,11 +1,39 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
 
+        while (true) {
+            System.out.print("Input the name of the book, empty spots: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Input the age recommendation: ");
+            int ageRecommendation = Integer.valueOf(scanner.nextLine());
+
+            books.add(new Book(name, ageRecommendation));
+            System.out.println("");
+        }
+        System.out.println("\n" + books.size() + " books in total.");
+        System.out.println("Books: ");
+
+        Comparator<Book> comparator = Comparator
+                .comparing(Book::getAgeRecommendation)
+                .thenComparing(Book::getName);
+
+        Collections.sort(books, comparator);
+
+        books.stream()
+                .forEach(x -> System.out.println(x.toString()));
     }
 
 }
