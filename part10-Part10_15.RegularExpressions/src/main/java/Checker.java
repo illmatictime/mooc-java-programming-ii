f4580279678a6bb9798761ab14d5347dcc111a2e
illmatictime <jerrylanderos@gmail.com>
2024-01-29 16:29:20 -0800

    Finished 10.15

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -2,4 +2,16 @@
 
 public class Checker {
 
+    public boolean timeOfDay(String string){
+        return string.matches("([01][01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]");
+        // return string.matches("([01][0123456789]|2[0123]):[012345][0123456789]:[012345][0123456789]");
+    }
+
+    public boolean allVowels(String string){
+        return string.matches("[aeiou]*");
+    }
+
+    public boolean isDayOfWeek(String string){
+        return string.matches("mon|tue|wed|thu|fri|sat|sun");
+    }
 }
