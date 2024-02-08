6214613fca02aced3b342487e2f0e263422da06d
illmatictime <jerrylanderos@gmail.com>
2024-02-07 21:16:27 -0800

    Finished 11.13

--- part11-Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
+++ part11-Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
@@ -1,11 +1,12 @@
 package dictionary;
 
 import java.util.Map.Entry;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class SaveableDictionary {
 
@@ -16,27 +17,33 @@ public class SaveableDictionary {
         this.translation = new HashMap<>();
     }
 
-    public SaveableDictionary(String file){
+    public SaveableDictionary(String file) {
         this();
         this.filename = file;
     }
 
-    public boolean save(){
+    public boolean save() {
         try {
-            System.out.println("need to setup");
-        } catch (Exception e) {
+            BufferedWriter filewriter = new BufferedWriter(new FileWriter(this.filename, false));
+            for (Entry<String, String> entry : this.translation.entrySet()) {
+                filewriter.write(entry.getKey() + ":" + entry.getValue());
+                // System.out.println(entry.getKey() + ":" + entry.getValue());
+                filewriter.newLine();
+            }
+            filewriter.close();
+        } catch (IOException e) {
             return false;
         }
         return true;
     }
-    
-    public boolean load(){
+
+    public boolean load() {
         try {
             Files.lines(Paths.get(this.filename))
-                .map(row -> row.split(":"))
-                .forEach(parts -> {
-                    this.translation.put(parts[0], parts[1]);
-                });
+                    .map(row -> row.split(":"))
+                    .forEach(parts -> {
+                        this.translation.put(parts[0], parts[1]);
+                    });
             return true;
         } catch (IOException e) {
             return false;
@@ -51,12 +58,12 @@ public class SaveableDictionary {
         for (Entry<String, String> aword : translation.entrySet()) {
             if (aword.getValue().equals(word)) {
                 return aword.getKey();
-                }
             }
+        }
         return this.translation.get(word);
     }
 
-    public void delete(String word){
+    public void delete(String word) {
         this.translation.remove(word);
         this.translation.remove(this.translate(word));
     }
