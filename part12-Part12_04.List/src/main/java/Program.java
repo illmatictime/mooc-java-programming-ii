4cdca62322321533dbcb674117b68515e989d500
illmatictime <jerrylanderos@gmail.com>
2024-02-09 16:35:27 -0800

    Finished 12.04

--- part12-Part12_04.List/src/main/java/Program.java
+++ part12-Part12_04.List/src/main/java/Program.java
@@ -1,11 +1,10 @@
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        // test your class here
 
     }
 
