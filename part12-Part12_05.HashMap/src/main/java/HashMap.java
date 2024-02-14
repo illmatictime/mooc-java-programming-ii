8875d5af1dba3265fc31527b5d6d9eec42a02f9b
illmatictime <jerrylanderos@gmail.com>
2024-02-13 22:07:03 -0800

    Finished 12.5

--- part12-Part12_05.HashMap/src/main/java/HashMap.java
+++ part12-Part12_05.HashMap/src/main/java/HashMap.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class HashMap<K, V> {
 
@@ -24,5 +25,79 @@ public class HashMap<K, V> {
             }
         }
         return null;
+    }
+
+    public void add(K key, V value) {
+        ArrayList<Pair<K, V>> valuesAtIndex = getListBasedOnKey(key);
+        int index = getIndexOfKey(valuesAtIndex, key);
+
+        if (index < 0) {
+            valuesAtIndex.add(new Pair<>(key, value));
+            this.firstFreeindex++;
+        } else {
+            valuesAtIndex.get(index).setValue(value);
+        }
+    }
+
+    private ArrayList<Pair<K, V>> getListBasedOnKey(K key) {
+        int hashValue = Math.abs(key.hashCode() % values.length);
+        if (values[hashValue] == null) {
+            values[hashValue] = new ArrayList<>();
+        }
+
+        return values[hashValue];
+    }
+
+    private int getIndexOfKey(List<Pair<K, V>> myList, K key) {
+        for (int i = 0; i < myList.size(); i++) {
+            if (myList.get(i).getKey().equals(key)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private void grow() {
+        // crete a new array
+        ArrayList<Pair<K, V>>[] newValues = new ArrayList[this.values.length * 2];
+
+        for (int i = 0; i < this.values.length; i++) {
+            // copy the values of the old array into the new one
+            copy(newValues, i);
+
+        }
+
+        // replace the old array with the new one
+        this.values = newValues;
+    }
+
+    private void copy(ArrayList<Pair<K, V>>[] newArray, int fromIdx) {
+        for (int i = 0; i < this.values[fromIdx].size(); i++) {
+            Pair<K, V> value = this.values[fromIdx].get(i);
+
+            int hashValue = Math.abs(value.getKey().hashCode() % newArray.length);
+            if (newArray[hashValue] == null) {
+                newArray[hashValue] = new ArrayList<>();
+            }
+
+            newArray[hashValue].add(value);
+        }
+    }
+
+    public V remove(K key) {
+        ArrayList<Pair<K, V>> valuesAtIndex = getListBasedOnKey(key);
+        if (valuesAtIndex.size() == 0) {
+            return null;
+        }
+
+        int index = getIndexOfKey(valuesAtIndex, key);
+        if (index < 0) {
+            return null;
+        }
+
+        Pair<K, V> pair = valuesAtIndex.get(index);
+        valuesAtIndex.remove(pair);
+        return pair.getValue();
     }
 }
