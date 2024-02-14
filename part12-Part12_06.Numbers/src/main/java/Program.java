7e5c3f5d873cccf0f1acae3b0fbc0851521f3a85
illmatictime <jerrylanderos@gmail.com>
2024-02-14 15:23:35 -0800

    Finished 12.06

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,24 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random ladyluck = new Random();
+
+        System.out.println("How many random numbers should be printed?");
+        while (true) {
+            String input = scanner.nextLine();
+
+            if(input.equals("quit")){
+                break;
+            }
+
+            int amount = Integer.valueOf(input);
+
+            for (int i = 0; i < amount; i++) {
+                int randomNumber = ladyluck.nextInt(11);
+                System.out.println(randomNumber);
+            }
+            
+        }
     }
 
 }
