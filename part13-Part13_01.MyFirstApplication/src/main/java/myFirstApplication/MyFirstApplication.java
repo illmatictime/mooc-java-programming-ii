205d05e8ab95747a7bb22c68caa350815e1ca390
illmatictime <jerrylanderos@gmail.com>
2024-02-26 21:40:43 -0800

    Update 13.13

--- part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
+++ part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
@@ -1,11 +1,13 @@
 package myFirstApplication;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MyFirstApplication {
-    
+public class MyFirstApplication extends Application {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Shit dont work.");
     }
 
 }
